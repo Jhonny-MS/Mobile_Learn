f58379c691437ca6e812316c66b58dff5e263ec5
Jhonny <jhmsouza10@gmail.com>
2021-09-16 11:07:35 -0300

    Add collectables coins and basic floor

---
+++ Assets/Scripts/Collectables/Collectable.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public string compareTag = "Player";
+    public ParticleSystem myParticleSystem;
+    public GameObject graficItem;
+    public float timeToHide = 0f;
+
+    [Header("Sounds")]
+    public AudioSource AudioSource;
+
+    private void Awake()
+    {
+       // if (myParticleSystem != null) myParticleSystem.transform.SetParent(null);
+    }
+    private void OnTriggerEnter(Collider collision)
+    {        
+        if (collision.transform.CompareTag(compareTag))
+        {
+            Collect();
+        }
+    }
+    protected virtual void Collect()
+    {
+        if (graficItem != null) graficItem.SetActive(false);
+        Invoke("HideObject", timeToHide);        
+        OnCollect();
+    }
+    private void HideObject()
+    {
+        gameObject.SetActive(false);
+    }
+    protected virtual void OnCollect()
+    {
+        if (myParticleSystem != null) myParticleSystem.Play();
+        if (AudioSource != null) AudioSource.Play();
+    }
+}
---
+++ Assets/Scripts/Collectables/ItemManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Master.Core.Singleton;
+
+public class ItemManager : Singleton<ItemManager>
+{
+    public SOInt coins;
+    public TextMeshProUGUI uiTextCoins;
+ 
+    private void Start()
+    {
+        Reset();
+    }
+    private void Reset()
+    {
+        coins.value = 0;
+        UpdateUI();
+    }
+    public void AddCoins(int amount = 1)
+    {
+        coins.value += amount;
+        UpdateUI();
+    }
+    private void UpdateUI()
+    {
+       // uiTextCoins.text = coins.value.ToString();
+    }
+
+}
